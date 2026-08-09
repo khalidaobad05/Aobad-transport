5de84cffad1e3aca4fe1d06bb18f6b3a088e0fea
Z User <z@container>
2026-08-09 11:23:22 +0000

    48c4e2f2-653b-4089-9d89-c310b11ee5f9

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,46 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
-import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: any
+  prisma: PrismaClient | undefined
 }
 
-let _db: any = undefined
+export const db = globalForPrisma.prisma ?? new PrismaClient()
 
-function getDb() {
-  if (_db) return _db
-
-  if (globalForPrisma.prisma) {
-    _db = globalForPrisma.prisma
-    return _db
-  }
-
-  if (process.env.TURSO_DATABASE_URL) {
-    const libsql = createClient({
-      url: process.env.TURSO_DATABASE_URL,
-      authToken: process.env.TURSO_AUTH_TOKEN,
-    })
-    const adapter = new PrismaLibSQL(libsql)
-    _db = new PrismaClient({ adapter })
-  } else {
-    _db = new PrismaClient()
-  }
-
-  if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = _db
-  }
-
-  return _db
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db
 }
-
-export const db = new Proxy({} as any, {
-  get(_target: any, prop: string | symbol, _receiver: any) {
-    const actualDb = getDb()
-    const value = Reflect.get(actualDb, prop, actualDb)
-    if (typeof value === 'function') {
-      return value.bind(actualDb)
-    }
-    return value
-  },
-})
